a47eaf8d2aef44cf8a3ff04beb2f1b15ef8bc651
Lucas Yang <lucasmf@yahoo-inc.com>
2017-06-19 15:30:43 +0800

    print more info

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,14 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.commons.vfs2.FileObject;
-import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.FileSystemManager;
 import org.apache.commons.vfs2.VFS;
 
 public class Sample {
 
-    public static void main(String[] args) throws FileSystemException {
-        String filePath = args[0];
+    public static void main(String[] args) throws IOException {
+        String filePathWithScheme = args[0];
         FileSystemManager fsManager = VFS.getManager();
-        FileObject file = fsManager.resolveFile(filePath);
+        FileObject file = fsManager.resolveFile(filePathWithScheme);
         System.out.println(file.exists());
+        String filePath = filePathWithScheme.substring(7);
+        File f = new File(filePath);
+        System.out.println(
+                "f exists" + f.exists() +
+                        ", is dir " + f.isDirectory() +
+                        ", is file " + f.isFile() +
+                        ", norm " + f.getCanonicalPath() +
+                        ", len " + f.length());
     }
 }
